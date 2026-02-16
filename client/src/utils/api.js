90fab856d546cd89ebea49cb2a96718aef9b190a
Shivam Goel <shivamkgjj2005@gmail.com>
2026-02-16 19:34:41 +0530

    Frontend API change

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { ethers } from 'ethers';
 import contractData from '../contractData.json';
 
-const API_URL = 'http://localhost:5000';
+const API_URL = 'https://certification-validation-backend-60s3.onrender.com/';
 
 // Use a public RPC or environment variable
 
